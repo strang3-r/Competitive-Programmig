90f2ffba500e7e725cd65d53c53ddb3dc309f280
Atmagyan Singh <55326048+Black-Devil007@users.noreply.github.com>
2021-10-25 15:58:28 +0530

    Update templete.cpp

--- templete.cpp
+++ templete.cpp
@@ -1,5 +1,3 @@
-// Buying_Tablet.cpp
-
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -26,7 +24,7 @@ using namespace std;
 #define ps(x, y)           fixed<<setprecision(y)<<x
 #define mk(arr,n,type)     type *arr = new type[n];
 #define w(t)               int x; cin>>x; while(x--)
-// mt19937                    rng(chrono::steady_clock::now,time_since_epoch(),count());
+mt19937                    rng(chrono::steady_clock::now,time_since_epoch(),count());
 typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
     
@@ -34,10 +32,10 @@ void file_i_o(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-/* <!-- #ifndef ONLINE_JUDGE
-			freopen("input.txt", "r", stdin);
-			freopen("output.txt", "w", stdout);
-		#endif --> */
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
 }
 
 int main(int argc, char const *argv[])
